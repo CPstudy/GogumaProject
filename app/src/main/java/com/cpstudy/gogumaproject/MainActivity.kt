aac0e9b01be264a91a96e35a3d0fa956276acfc0
김성재 <guide94@naver.com>
2021-01-20 22:59:09 +0900

    GPS 기반 위치 정보 가져오기
    * `랜덤 추천` 버튼을 누르면 현재 위치로 지도 이동

--- app/src/main/java/com/cpstudy/gogumaproject/MainActivity.kt
+++ app/src/main/java/com/cpstudy/gogumaproject/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.cpstudy.gogumaproject
 
 import android.Manifest
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.cpstudy.gogumaproject.databinding.ActivityMainBinding
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -27,10 +33,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     private lateinit var mMap: GoogleMap
 
-    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            isGranted: Boolean ->
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
         if(isGranted) {
             // Permission is granted. Contimue the action of workflow in your app.
             Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
@@ -50,13 +57,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.lifecycleOwner = this
         setContentView(binding.root)
 
+        LocationServices.getFusedLocationProviderClient(this)
         checkPermission()
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         binding.btnSuggest.setOnClickListener {
-            checkPermission()
+            myLocation()
         }
     }
 
@@ -64,21 +72,81 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney")
+        mMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
         )
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
-    private fun checkPermission() {
+    private fun createLocationRequest() {
+        val locationRequest = LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = locationRequest?.let {
+            LocationSettingsRequest.Builder().addLocationRequest(it)
+        }
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder?.build())
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+            checkPermission()
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(this@MainActivity, 100)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    private val locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            var longitude = locationResult.lastLocation.longitude
+            var latitude = locationResult.lastLocation.latitude
+            Log.d("GPS", "$latitude / $longitude")
+            fusedLocationClient.removeLocationUpdates(this)
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(latitude, longitude)))
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
+        }
+
+        override fun onLocationAvailability(locationAvailability: LocationAvailability) {
+            super.onLocationAvailability(locationAvailability)
+
+        }
+    }
+
+    private fun myLocation() {
+        checkPermission()
+        createLocationRequest()
+    }
+
+    private fun checkPermission(): Boolean {
 
         when {
             ContextCompat.checkSelfPermission(
                 applicationContext,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED -> {
-
+                Log.d("GPS", "위치 권한 확인")
+                return true
             }
             shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
                 // In an educational UI, explain to the user why your app requires this
@@ -95,5 +163,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
             }
         }
+
+        return false
     }
 }
