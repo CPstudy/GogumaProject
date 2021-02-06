e889f0ce38b04d294cafeb8d4d85cbce7d5327b1
김성재 <guide94@naver.com>
2021-02-06 21:28:06 +0900

    코드 리팩토링 및 식당 정보 불러오기
    * retrofit 적용
    * LocationHelper의 함수를 전역 함수로 바꿈
    * 식당 관련 데이터 클래스 생성
    * 이전에 없어졌던 퍼미션 체크 다이얼로그 다시 추가

--- app/src/main/java/com/cpstudy/gogumaproject/LocationHelper.kt
+++ app/src/main/java/com/cpstudy/gogumaproject/LocationHelper.kt
@@ -1,62 +1,150 @@
 package com.cpstudy.gogumaproject
 
 import android.Manifest
-import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
-import androidx.core.content.ContextCompat
+import com.cpstudy.gogumaproject.retro.Restaurant
+import com.cpstudy.gogumaproject.retro.RestaurantAPI
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.tasks.Task
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
-class LocationHelper(private val activity: Activity) {
+/**
+ * 현재 위치의 위도와 경도 값을 구해 [onSuccess] 수행
+ *
+ * @param onSuccess 위치 권한 허용 후 실행할 함수
+ */
+fun requestDeviceLocation(context: Context, onSuccess: (LatLng) -> Unit) {
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var callback: (LatLng) -> Unit
+    val locationRequest = LocationRequest.create()
+            .setInterval(10000)
+            .setFastestInterval(5000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
 
-    fun location(callback: (LatLng) -> (Unit)) {
-        this.callback = callback
-        LocationServices.getFusedLocationProviderClient(activity)
-
-        val locationRequest = LocationRequest.create().apply {
-            interval = 10000
-            fastestInterval = 5000
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
-
-        val builder = locationRequest.let {
-            LocationSettingsRequest.Builder().addLocationRequest(it)
-        }
-
-        val client: SettingsClient = LocationServices.getSettingsClient(activity)
-        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder?.build())
-        task.addOnSuccessListener {
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
-            checkPermission()
-            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
-        }
-
-    }
-
-    private val locationCallback: LocationCallback = object: LocationCallback() {
+    val locationCallback: LocationCallback = object: LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
-            super.onLocationResult(locationResult)
             val latitude = locationResult.lastLocation.latitude
             val longitude = locationResult.lastLocation.longitude
             fusedLocationClient.removeLocationUpdates(this)
-            callback(LatLng(latitude, longitude))
+            onSuccess(LatLng(latitude, longitude))
+            getRestaurants(context, LatLng(latitude, longitude))
         }
     }
 
-    private fun checkPermission() {
-        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(activity.applicationContext, "권한이 없습니다. 안타깝습니다.", Toast.LENGTH_LONG).show()
-            return
-        }
+    LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+            .build()
+            .let {
+                LocationServices.getSettingsClient(context).checkLocationSettings(it)
+            }.addOnSuccessListener {
+                if(checkPermission(context)) {
+                    fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+                } else {
+                    Toast.makeText(context, "권한 설정을 하세요.", Toast.LENGTH_LONG).show()
+                }
+            }
+}
+
+private fun getRestaurants(context: Context, latLng: LatLng) {
+    val retro = Retrofit.Builder()
+        .baseUrl("https://maps.googleapis.com/")
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val api = retro.create(RestaurantAPI::class.java)
+//    val callGetRestaurant = api.getNearRestaurants(
+//        "AIzaSyAZlSgYZO3kimuB7VQC0xddYJWUrQfs1gE",
+//        "restaurant",
+//        "37.4743187, 127.1509092",
+//        1000
+//    )
+//
+//    testFunction1 {
+//        testFunction2 {
+//            callGetRestaurant.enqueue(object: Callback<Restaurant> {
+//                override fun onResponse(call: Call<Restaurant>, response: Response<Restaurant>) {
+//                    Log.d("retrofit", "성공: ${response.body()}")
+//                }
+//
+//                override fun onFailure(call: Call<Restaurant>, t: Throwable) {
+//                    Log.d("retrofit", "실패: $t")
+//                }
+//
+//            })
+//        }
+//    }
+
+
+//    testFuntionObservable1()
+//        .subscribeOn(Schedulers.io())
+//        .flatMap {
+//            testFuntionObservable2()
+//        }.flatMap {
+//            testFuntionObservable2()
+//        }.flatMapSingle {
+//            callGetRestaurant
+//        }
+//        .observeOn(AndroidSchedulers.mainThread())
+//        .doOnNext {
+//            Log.d("rxkotlin", "성공: $it")
+//            Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show()
+//        }
+//        .doOnError {
+//            Log.e("rxkotlin", it.toString())
+//        }
+//        .subscribe()
+
+    GlobalScope.launch { 
+        val result1 = testFuntionObservable1().blockingFirst()
+        val result2 = testFuntionObservable1().blockingFirst()
+        val restaurant = api.getNearRestaurants(
+            "AIzaSyAZlSgYZO3kimuB7VQC0xddYJWUrQfs1gE",
+            "restaurant",
+            "37.4743187, 127.1509092",
+            1000
+        )
     }
+}
+private fun testFuntionObservable1(): Observable<Int> {
+    return Observable.fromCallable {
+        Thread.sleep(1000)
+        10
+    }
+}
+
+private fun testFuntionObservable2(): Observable<Int> {
+    return Observable.fromCallable {
+        Thread.sleep(1000)
+        10
+    }
+}
+
+private fun testFunction1(callback: (Int) -> Unit) {
+    Thread.sleep(1000)
+    callback(10)
+}
+
+private fun testFunction2(callback: (Int) -> Unit) {
+    Thread.sleep(1000)
+    callback(10)
+}
+
+private fun checkPermission(context: Context): Boolean {
+    return !(ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
 }
--- app/src/main/java/com/cpstudy/gogumaproject/MainActivity.kt
+++ app/src/main/java/com/cpstudy/gogumaproject/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.cpstudy.gogumaproject
 
 import android.Manifest
-import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -14,15 +11,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.cpstudy.gogumaproject.databinding.ActivityMainBinding
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.tasks.Task
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -56,12 +50,13 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.model = viewModel
         binding.lifecycleOwner = this
         setContentView(binding.root)
+        checkPermission()
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         binding.btnSuggest.setOnClickListener {
-            LocationHelper(this).location {
+            requestDeviceLocation(this) {
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(it.latitude, it.longitude)))
                 mMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
             }
@@ -71,9 +66,33 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        LocationHelper(this).location {
+        requestDeviceLocation(this) {
             mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(it.latitude, it.longitude)))
             mMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
+        }
+    }
+
+    private fun checkPermission() {
+
+        when {
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED -> {
+
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for the a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                Log.e("TAG", "권한을 해야합니다.")
+                Toast.makeText(applicationContext, "권한을 설정해야합니다.", Toast.LENGTH_SHORT).show()
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
         }
     }
 }
